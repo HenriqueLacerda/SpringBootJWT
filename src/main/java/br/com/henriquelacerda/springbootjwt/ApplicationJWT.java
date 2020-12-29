c7d38ae13f2812ba521acd9e74b3827208a5acbc
Geraldo H. Lacerda <geraldohenriquelacerda@gmail.com>
2020-12-29 19:17:38 -0300

    Configuracao inicial do projeto

---
+++ src/main/java/br/com/henriquelacerda/springbootjwt/ApplicationJWT.java
@@ -0,0 +1,13 @@
+package br.com.henriquelacerda.springbootjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationJWT {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationJWT.class, args);
+    }
+
+}
