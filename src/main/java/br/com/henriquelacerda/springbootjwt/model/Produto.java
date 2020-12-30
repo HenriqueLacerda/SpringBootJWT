ff807f6d28056b3b13affb364911162db0718ad9
Geraldo H. Lacerda <geraldohenriquelacerda@gmail.com>
2020-12-30 20:46:33 -0300

    Correção da tabela de produtos

--- src/main/java/br/com/henriquelacerda/springbootjwt/model/Produto.java
+++ src/main/java/br/com/henriquelacerda/springbootjwt/model/Produto.java
@@ -3,14 +3,15 @@ package br.com.henriquelacerda.springbootjwt.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 
-@Entity(name = "produto")
+@Entity(name = "produtos")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Produto {
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
